f1fd45b73f1a3e00aac360c7127701b1e869f364
Matan Ben Ishay <matan12333445@gmail.com>
2025-01-08 23:05:22 +0200

    new commit

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -1,13 +1,23 @@
-package com.myapp;
+// package com.myapp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World from Matan!" );
+// /**
+//  * Hello world!
+//  *
+//  */
+// public class App 
+// {
+//     public static void main( String[] args )
+//     {
+//         System.out.println( "Hello World from Matan!" );
+//     }
+// }
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Matan!");
+        try {
+            Thread.sleep(Long.MAX_VALUE); // Keep the application running
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
